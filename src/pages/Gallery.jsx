e6aa479fce27f2caa593a8999a46255b54482d6a
MLA-TechTeam <136065389+MLA-TechTeam@users.noreply.github.com>
2026-07-02 22:24:03 +0530

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -69,6 +69,7 @@ export default function Gallery() {
     };
 
     loadAllImages();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filteredItems = selectedCategory === 'all' 
